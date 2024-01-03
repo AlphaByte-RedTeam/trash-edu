5cd17f0ab2d5d9c3d4947145caed889da64b081a
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 17:09:44 +0700

    add waste items list

---
+++ data/wasteItems.ts
@@ -0,0 +1,6 @@
+export const wasteItems = [
+    { id: 1, img: "/img/organic-waste.svg", title: "Organic Waste", desc: "Organic waste, derived from plant and animal matter, includes food scraps and yard waste.", bgColor: "#606264", },
+    { id: 2, img: "/img/plastic-waste.svg", title: "Plastic Waste", desc: "non-biodegradable nature From single-use packaging to durable goods.", bgColor: "#FF9D06", },
+    { id: 3, img: "/img/paper-waste.svg", title: "Paper Waste", desc: "Paper waste, stemming from various sources like packaging, office materials, and newspapers.", bgColor: "#008BCA", },
+    { id: 4, img: "/img/glass-waste.svg", title: "Glass Waste", desc: "Glass recycling conserves energy and raw materials, contributing to sustainability efforts.", bgColor: "#02CE6F", },
+]
