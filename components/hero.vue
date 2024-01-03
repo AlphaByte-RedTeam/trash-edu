2f7fbb49fe4c697ff6fecaddd5d003693432156b
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 16:08:09 +0700

    add hero section

---
+++ components/hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="hero" class="flex flex-row px-32 py-8 justify-between">
+        <div id="jargon" class="flex flex-col gap-4">
+            <h1 class="font-medium text-[150px] w-6 text-secondary">
+                Reduce, Reuse, Recycle
+            </h1>
+            <p id="caption" class="text-3xl">
+                Empower green technology through education
+            </p>
+        </div>
+        <NuxtImg src="/img/hero.svg" width="700" height="700" />
+    </div>
+</template>
