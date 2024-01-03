026dbbf62d72922e5e52a0f8530aa3b3602b74c7
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 11:35:36 +0700

    add tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@formkit/auto-animate", "radix-vue", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@formkit/auto-animate",
+    "radix-vue",
+    "nuxt-icon",
+  ],
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
