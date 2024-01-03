eb157b681ded589504bf631998d4820159e1aca4
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 18:10:26 +0700

    add waste sort draggable

---
+++ components/wasteSort.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="px-32">
+        <dragggable>
+            
+        </dragggable>
+    </div>
+</template>
+
+<!-- Reference: https://sortablejs.github.io/Vue.Draggable/#/two-lists -->
