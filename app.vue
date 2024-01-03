faae22af8a6d31bb1213aa7c7172a4ece9ab3cb7
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 17:55:40 +0700

    add main section

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script lang="ts">
+const layout = () => import("~/layouts/default.vue")
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col justify-between min-h-screen bg-primary">
+    <NuxtLayout :name="layout">
+      <div id="content">
+        <Hero />
+        <WatchVideo />
+        <WasteTypes />
+      </div>
+    </NuxtLayout>
   </div>
 </template>
