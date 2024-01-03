9cd8de93646b8a29e9ef6b1c9bac36e82800172c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 17:47:00 +0700

    remove dialog close

--- components/wasteCard.vue
+++ components/wasteCard.vue
@@ -55,11 +55,6 @@ export default {
                         <li>{{ step3 }}</li>
                     </ul>
                 </DialogDescription>
-                <DialogClose
-                    class="text-grass11 hover:bg-green4 focus:shadow-green7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-                    aria-label="Close"
-                >
-                </DialogClose>
             </DialogContent>
         </DialogPortal>
     </DialogRoot>
