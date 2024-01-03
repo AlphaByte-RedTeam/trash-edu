44d90658ed7d7f63f53895ef16e956fdbfeaca10
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 17:45:43 +0700

    use recycling step props

--- components/wasteTypes.vue
+++ components/wasteTypes.vue
@@ -14,7 +14,7 @@ export default {
     <div class="px-32 py-16 flex flex-col justify-center gap-8 align-middle items-center">
         <h1 class="capitalize text-5xl text-center">Different Types Of Waste</h1>
         <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <WasteCard v-for="wasteItem in wasteItems" :key="wasteItem.id" :bg-color="wasteItem.bgColor" :title="wasteItem.title" :img-src="wasteItem.img" :title-name="wasteItem.title" :desc="wasteItem.desc" />
+            <WasteCard v-for="wasteItem in wasteItems" :key="wasteItem.id" :bg-color="wasteItem.bgColor" :title="wasteItem.title" :img-src="wasteItem.img" :title-name="wasteItem.title" :desc="wasteItem.desc" :step1="wasteItem.firstStep" :step2="wasteItem.secondStep" :step3="wasteItem.thirdStep" />
         </div>
     </div>
 </template>
