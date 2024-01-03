df4ca8a9896293a79185d75bcce9f89dbadc6bd7
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 14:11:14 +0700

    add logo component

---
+++ components/logo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="logo flex flex-row gap-2">
+        <NuxtImg src="/img/logo.svg" height="48" width="48"  />
+        <p class="text-4xl text-accent font-bold">3R</p>
+    </div>
+</template>
