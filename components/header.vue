4db8f7a4b5228f5b9570b9ee98fd90df77b47f1c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 14:11:24 +0700

    modify logo component

--- components/header.vue
+++ components/header.vue
@@ -12,10 +12,7 @@ export default {
 
 <template>
     <div class="w-full py-6 px-32 text-2xl bg-secondary flex flex-row justify-between">
-        <div class="logo flex flex-row gap-2">
-            <NuxtImg src="/img/logo.svg" height="48" width="48"  />
-            <p class="text-4xl text-accent font-bold">3R</p>
-        </div>
+        <Logo />
         <div class="menus">
             <ul class="flex flex-row gap-8">
                 <li class="text-accent font-medium" v-for="menuItem in menuItems" :key="menuItem.id">
