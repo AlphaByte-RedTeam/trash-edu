e94ea75e02315cff1ada3a154d26530e52a47b72
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 16:36:25 +0700

    add watch video section

---
+++ components/watchVideo.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="px-32 py-16 flex flex-col justify-center gap-8 align-middle items-center">
+        <h1 class="text-5xl font-medium text-center capitalize">
+            Watch the video
+        </h1>
+        <video controls poster="/img/video-poster.png" width="1024">
+            <source src="/vid/recycling.mp4" type="video/mp4" />
+        </video>
+    </div>
+</template>
