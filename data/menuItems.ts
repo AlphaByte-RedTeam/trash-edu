1ccd7e2b0d7fb76a4bb41579b02c10c58a4d60da
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-03 13:54:17 +0700

    add menu items list

---
+++ data/menuItems.ts
@@ -0,0 +1,6 @@
+export const menuItems = [
+  {id: 1, label: 'Home', path: '#'},
+  {id: 2, label: 'About', path: '#'},
+  {id: 3, label: 'Games', path: '#'},
+  {id: 4, label: 'Educate', path: '#'}
+]
