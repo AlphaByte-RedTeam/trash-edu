8a2e44fe298d625dffbcbd4bd7074b3f72cab243
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-04 12:50:55 +0700

    add waste sort items

---
+++ data/wasteSortItems.ts
@@ -0,0 +1,12 @@
+export const wasteSortItems = [
+    { id: 1, labelName: 'Banana Skin', bgColor: "#606264", material: "Organic" },
+    { id: 2, labelName: 'Apple Core', bgColor: "#606264", material: "Organic" },
+    { id: 3, labelName: 'Beer Bottle', bgColor: "#02CE6F", material: "Glass" },
+    { id: 4, labelName: 'Water Bottle', bgColor: "#FF9D06", material: "Plastic" },
+    { id: 5, labelName: 'Newspaper', bgColor: "#008BCA", material: "Paper" },
+    { id: 6, labelName: 'Coffee Grounds', bgColor: "#606264", material: "Organic" },
+    { id: 7, labelName: 'Plastic Bag', bgColor: "#FF9D06", material: "Plastic" },
+    { id: 8, labelName: 'Cardboard Box', bgColor: "#008BCA", material: "Paper" },
+    { id: 9, labelName: 'Glass Jar', bgColor: "#02CE6F", material: "Glass" },
+    { id: 10, labelName: 'Orange Peel', bgColor: "#606264", material: "Organic" },
+];
